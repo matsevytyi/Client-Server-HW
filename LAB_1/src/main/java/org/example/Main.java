43946a31978342ef67ab7e30558bb3fabef1be64
Andrii Matsevytyi <macevitiya@i.ua>
2024-05-26 00:04:08 +0300

    Made initial integration test in main

---
+++ LAB_1/src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CustomKey key = new CustomKey();
+        System.out.println(key.getSecretKey().getAlgorithm());
+        Packet packet = new Packet((byte) 1, new byte[]{1, 2, 3, 4, 5, 6, 7, 8}, new Message("ping", "1234", new byte[]{1, 2, 3, 4, 5, 6, 7, 8}).toBytes());
+        byte[] encoded = PacketEncoder.encodePacket(packet, key.getSecretKey());
+        for (byte b : encoded) {
+            System.out.print((char) b + " ");
+        }
+
+        System.out.println("\n-------------------\n");
+
+        byte[] bytes = packet.toBytes();
+       Packet decoded = PacketDecoder.decodePacket(bytes, key.getSecretKey());
+       for (byte b : packet.toBytes()) {
+           System.out.print( (char) b + " ");
+       }
+
+       System.out.println("\n-------------------\n");
+       Message message = decoded.getMessage();
+       byte[] messageBytes = message.toBytes();
+       for (byte b : messageBytes) {
+           System.out.print( (char) b + " ");
+       }
+
+    }
+}
