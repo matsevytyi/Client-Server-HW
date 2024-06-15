33d34a46e29200b0077075938412f4bc667ed50a
Andrii Matsevytyi <macevitiya@i.ua>
2024-06-15 11:48:30 +0300

    Created Item entity to properly unpack and pass items, received from database. Also dublicated Message entity from LAB_1.

---
+++ HW_4/src/main/java/org/example/Item.java
@@ -0,0 +1,25 @@
+package org.example;
+
+//Entity class fort item
+public class Item {
+
+    private String name;
+    private int price;
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String toString() {
+        return name + " " + price;
+    }
+}
---
+++ HW_4/src/main/java/org/example/Message.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class Message {
+
+    private int cType;
+    private int bUserId;
+    private String message;
+
+    public Message(int cType, int userID, String message) {
+        this.cType = cType;
+        this.bUserId = userID;
+        this.message = message;
+    }
+
+    public Message(byte[] bytes) {
+        this.cType = ByteBuffer.wrap(bytes, 0, 4).getInt();
+        bUserId = ByteBuffer.wrap(bytes, 4, 4).getInt();
+        message = new String(Arrays.copyOfRange(bytes, 8, bytes.length));
+    }
+
+    public String toString() {
+        return "Message{" +
+                "cType=" + cType +
+                ", bUserId=" + bUserId +
+                ", message='" + message + '\'' +
+                '}';
+    }
+
+    public byte[] toBytes() {
+        byte[] messageBytes = message.getBytes(StandardCharsets.UTF_8); // Convert message to byte array
+
+        ByteBuffer buffer = ByteBuffer.allocate(8 + messageBytes.length);
+        buffer.putInt(cType);           // Put cType as 4 bytes
+        buffer.putInt(bUserId);         // Put bUserId as 4 bytes
+        buffer.put(messageBytes);       // Put message bytes
+
+        return buffer.array();          // Get the underlying byte array
+    }
+
+
+    public int getcType() {
+        return cType;
+    }
+
+    public int getbUserId() {
+        return bUserId;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
